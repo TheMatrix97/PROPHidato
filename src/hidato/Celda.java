1a92cb91f11bc7dfa45e74ceea719d822c62bc7a
marc.catrisse <marc.catrisse@a5s102pc44.fib.upc.es>
2018-03-05 15:45:39 +0100

    First commit celda class add + tests

---
+++ src/hidato/Celda.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hidato;
+
+/**
+ *
+ * @author marc.catrisse
+ */
+public class Celda {
+    boolean valida; //true si es valida, se puede usar
+    boolean prefijada; //true si es prefijada
+    boolean vacia; //true si esta vacia
+    int valor;
+    Celda[] vecinos;
+    //constructores
+    public Celda(boolean prefijada, int valor) { //celda valida con valor prefijada o no
+        this.valida = true;
+        this.prefijada = prefijada;
+        this.vacia = false;
+        this.valor = valor;
+    }
+
+    public Celda(boolean vacia) { //celda sin valor pero valida
+        this.valida = true;
+        this.prefijada = false;
+        this.vacia = vacia;
+    }
+    
+    public Celda() { //celda no valida
+        this.valida = false;
+    }
+    
+    //getter
+    public boolean isValida() {
+        return valida;
+    }
+
+    public boolean isPrefijada() {
+        return prefijada;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public boolean isVacia() {
+        return vacia;
+    }
+
+    public Celda[] getVecinos() {
+        return vecinos;
+    }
+    
+    //setter
+    public void setVecinos(Celda[] vecinos) {
+        this.vecinos = vecinos;
+    }
+    
+}
---
+++ test/hidato/CeldaTest.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hidato;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author marc.catrisse
+ */
+public class CeldaTest {
+    
+    public CeldaTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of isValida method, of class Celda.
+     */
+    @Test
+    public void testIsValida() {
+        System.out.println("isValida");
+        Celda instance = new Celda(false,5); //
+        boolean expResult = true;
+        boolean result = instance.isValida();
+        assertEquals(expResult, result);
+
+    }
+
+    /**
+     * Test of isPrefijada method, of class Celda.
+     */
+    @Test
+    public void testIsPrefijada() {
+        System.out.println("isPrefijada");
+        Celda instance = new Celda(true,6);
+        boolean expResult = true;
+        boolean result = instance.isPrefijada();
+        assertEquals(expResult, result);
+
+    }
+
+    /**
+     * Test of getValor method, of class Celda.
+     */
+    @Test
+    public void testGetValor() {
+        System.out.println("getValor");
+        Celda instance = new Celda(false, 90);
+        int expResult = 90;
+        int result = instance.getValor();
+        assertEquals(expResult, result);
+
+    }
+
+    /**
+     * Test of isVacia method, of class Celda.
+     */
+    @Test
+    public void testIsVacia() {
+        System.out.println("isVacia");
+        Celda instance = new Celda(false,90);
+        boolean expResult = false;
+        boolean result = instance.isVacia();
+        assertEquals(expResult, result);
+        
+    }
+
+    /**
+     * Test of getVecinos method, of class Celda.
+     */
+    @Test
+    public void testGetVecinos() {
+        System.out.println("getVecinos");
+        Celda instance = new Celda();
+        Celda[] expResult = null;
+        Celda[] result = instance.getVecinos();
+        assertArrayEquals(expResult, result);
+
+    }
+
+    /**
+     * Test of setVecinos method, of class Celda.
+     */
+    @Test
+    public void testSetVecinos() {
+        System.out.println("setVecinos");
+        Celda[] vecinos = null;
+        Celda instance = new Celda();
+        instance.setVecinos(vecinos);
+        Celda[] result = instance.getVecinos();
+        assertArrayEquals(result,vecinos);
+    }
+    
+}
