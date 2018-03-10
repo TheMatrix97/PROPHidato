7fd30eada877ba4245a5b306a87a91265c8b2ccc
Marc Catrisse <marc@catrisse.net>
2018-03-10 22:43:21 +0100

    Update Hidato.java

--- src/hidato/Hidato.java
+++ src/hidato/Hidato.java
@@ -15,8 +15,7 @@ public class Hidato {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello world");
-      
+        System.out.println("hello wor;
         // TODO code application logic here
     }
     
