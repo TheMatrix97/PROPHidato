f1c9425cf8ab1bd9b1dc808b7077a96d8abd46aa
Marc Catrisse <marc@catrisse.net>
2018-03-10 22:11:01 +0100

    Update Hidato.java

--- src/hidato/Hidato.java
+++ src/hidato/Hidato.java
@@ -16,6 +16,7 @@ public class Hidato {
      */
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world2");
         // TODO code application logic here
     }
     
