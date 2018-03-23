a74ecb65dce671a8f06ef9aa010e3b4807716d6d
Marc Catrisse Perez <marc.catrisse@a5s102pc42.fib.upc.es>
2018-03-23 17:48:45 +0100

    revisar ranking de toni

--- src/hidato/Ranking.java
+++ src/hidato/Ranking.java
@@ -39,7 +39,39 @@ public class Ranking{
             actualizar_peor_tiempo();
         }
     }
+    //ERROR A REVISAR
     private void actualizar_peor_tiempo(){
-        
+        peorRecord = records[0].clone();
+        for (int i = 1; i < records.size();i++){
+            if (records[i].getTime().get_time_millis() > peorRecord.getTime().get_time_millis()){
+                peorRecord = records[i];
+            } 
+        }
+    }
+    private void ordenar_para_mostrar(){
+        new List<Record> res = records.clone();
+        mergesort (res,0,res.size());
+    }
+
+
+    private void mergesort (List <Record> res, int l, int r){
+        if (l < r) {
+            int m = (l + r)/2;
+            mergesort(res,l,m);
+            mergesort(res,l,m);
+            merge(res, l,m, r);
+        }
+    }
+
+    private void merge(List <Record> res, int l, int m, int r){
+       List <Record> b(r-l+1);
+       int i = l, j = m +1, k = 0;
+       while (i <= m and j <= r){
+           if (v[i].getTime().get_time_millis() <= res[j].getTime().get_time_millis()) b[k++] = v[i++]
+           else b[k++] = res[j++];
+       }
+       while (i <= m) b[k++] = res[i++];
+       while (j <= r) b[k++] = res[j++];
+       for(k = 0; k < r - l; ++k) res[l+k] = b[k]
     }
 }
