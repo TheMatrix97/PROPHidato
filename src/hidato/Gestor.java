529baf8aea3cf04b27b7f4ac9ac2abbe83019558
AntonioGuileraDomingo <toniguilera@hotmail.com>
2018-03-30 21:13:52 +0200

    Un poco de plantilla de lo q será gestor

---
+++ src/hidato/Gestor.java
@@ -0,0 +1,20 @@
+package hidato;
+
+
+import java.util.ArrayList;
+/* @author antonio.guilera & marc.blanca */
+
+public class Gestor {
+    //Comprova Save
+    //Consulta Ranking
+    //Guardar Partida
+    //Cargar Partida
+    private Ranking rank;
+    private Partida game;
+    private static final Gestor GestorHidato;
+    //La clase Gestor es de tipo Singleton, es decir q hay solo una instancia de gestor
+    //Para conseguir que una clase sea de tipo Singleton necesitamos en primer lugar que su constructor sea privado.
+    // De esa forma ningún programa será capaz de construir objetos de esta tipo .
+    // En segundo lugar necesitaremos disponer de una variable estatica privada que almacene una referencia al objeto que vamos a crear a traves del constructor .
+    // Por ultimo un método estático publico que se encarga de instanciar el objeto la primera vez y almacenarlo en la variable estática.
+}
