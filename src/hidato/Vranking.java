be40f592f3d067a08003c526f0ce00f6e30b1418
AntonioGuileraDomingo <toniguilera@hotmail.com>
2018-05-14 21:35:45 +0200

    Hecho lo de la representación en tabla del ranking, pero no vemos como estan guardados los rankings como para probarlo, no estamos seguros de si añade bien a.a

--- src/hidato/Vranking.java
+++ src/hidato/Vranking.java
@@ -1,56 +1,87 @@
 package hidato;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Vranking {
     public JPanel panel1;
     private JPanel southPanel;
     private JButton tornarButton;
-    private JButton OKButton;
     public JLabel label1;
     public JLabel label2;
     public JLabel label3;
+    private JTable table1;
+    private JLabel labelr;
+
 
 
     public Vranking(){
+        String s = label1.getText();
+        DefaultTableModel modelo = (DefaultTableModel)table1.getModel();
+        char forma =' ';
+        switch (s) {
+            case "Quadrat":
+                forma = 'Q';
+            case "Triangle":
+                forma = 'T';
+            case  "Hexagon":
+                forma = 'H';
+        }
+        String s2 = label2.getText();
+        String adj ="";
+        switch (s2) {
+            case "Costats":
+                adj = "C";
+            case "Costats+angles":
+                adj = "CA";
+        }
+        String dificult = label3.getText();
+        Configuracio c = new Configuracio(dificult,adj,forma);
+        Object [] filaux = new Object[2];
+        filaux[0] = "NomJugador";
+        filaux[1] = "Temps";
+        modelo.addRow ( filaux );
+
+        try {
+            StringBuilder sb = new StringBuilder();
+            GestorSaves gs = new GestorSaves();
+            ArrayList<Ranking> ar = gs.cargar_ranking();
+            boolean b = false;
+            for(Ranking a : ar){
+                Configuracio confi = a.getConf();
+                if (confi.equals(c)){
+                    b = true;
+                    ArrayList<Record> rl = a.getRecords();
+                    for (Record r : rl){
+                         Object [] fila = new Object[2];
+                        fila[0] = r.getnomJugador();
+                        fila[1] = r.getTime();
+                        modelo.addRow ( fila ); // Añade una fila al final
+                        }
+                }
+                if (b) break;
+            }
+        } catch (Exception e) {
+
+        }
+
+
         tornarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 CtrlPresentacio.getSingletonInstance().setContentFrame(new SelectRanking().getPanel());
             }
+
         });
-    //    this.filesPath = new File("").getAbsolutePath() + "/saves/";
-        String rank = new String();
-        String path = new File("").getAbsolutePath() + "/saves/ranking.hidato";
-        File archivo = new File(path);
 
-        FileReader fr = null;
-        BufferedReader entrada = null;
-        try {
-            fr = new FileReader(path);
-            entrada = new BufferedReader(fr);
 
-            while(entrada.ready()){
-                rank += entrada.readLine();
-            }
-
-        }catch(IOException e) {
-            e.printStackTrace();
-        }finally{
-            try{
-                if(null != fr){
-                    fr.close();
-                }
-            }catch (Exception e2){
-                e2.printStackTrace();
-            }
-        }
     }
 
     public JPanel getPanel() {
